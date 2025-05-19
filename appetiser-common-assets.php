d95936a7879da2a402a9ab38ba2927aae06413e2
Norbert Feria <norbert.feria@gmail.com>
2025-05-19 18:11:09 +0800

    adds requirement for common assets plugin

--- appetiser-common-assets.php
+++ appetiser-common-assets.php
@@ -1,1 +1,11 @@
 <?php 
+/**
+ * Plugin Name: Appetiser Common Assets
+ * Plugin URI:  https://appetiser.com.au
+ * Description: Shared resources and functions used by other Appetiser plugins for Dashboard UI.
+ * Version: 1.0.0
+ * Author: Landing page team
+ * Author URI: https://appetiser.com.au
+ * License: GPL v3
+*  License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ */
