6c7b8bc0a40a2d2006e898db62b1b782ed685d43
Norbert Feria <norbert.feria@gmail.com>
2025-05-20 16:03:58 +0800

    add appetiser tools on admin dashboard menu

--- appetiser-common-assets.php
+++ appetiser-common-assets.php
@@ -9,3 +9,20 @@
  * License: GPL v3
 *  License URI: https://www.gnu.org/licenses/gpl-3.0.html
  */
+
+
+ add_action('admin_menu', function() {
+    add_menu_page(
+        'Appetiser Tools',           // Page title
+        'Appetiser Tools',           // Menu title
+        'manage_options',            // Capability
+        'appetiser-tools',           // Menu slug
+        '__return_null',             // Callback (null, since submenus will handle content)
+        'dashicons-admin-generic',  // Icon
+        30                           // Position
+    );
+});
+
+add_action('admin_menu', function() {
+    remove_submenu_page('appetiser-tools', 'appetiser-tools');
+}, 999); // Priority must be after the main menu is added
